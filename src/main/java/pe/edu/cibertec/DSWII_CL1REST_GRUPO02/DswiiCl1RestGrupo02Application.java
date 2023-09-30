94b66e68e08b2a6177070461e5707ee4c774bf0a
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-09-29 21:40:53 -0500

    Primer Commit REST

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1REST_GRUPO02/DswiiCl1RestGrupo02Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1REST_GRUPO02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1RestGrupo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1RestGrupo02Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL1REST_GRUPO02/DswiiCl1RestGrupo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1REST_GRUPO02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1RestGrupo02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
